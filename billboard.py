f9aa1101963c5cf886374f582327708920072a28
bella-ruiz <bellabruiz@gmail.com>
2022-03-04 20:01:09 -0500

    Added writing function

--- billboard.py
+++ billboard.py
@@ -9,29 +9,38 @@ url_list = ['/2021-03-06/','2020-03-06/', '2019-03-06/', '2018-03-06/', '2017-03
 
 filename = 'fullscrape.txt'
 myfile = open(filename, 'w')
-list = [ ]
+
 
 def scrape_chart(chart_url):
+    big_list = [ ]
     url = 'https://www.billboard.com/charts/hot-100' + chart_url
     page = requests.get(url)
     soup = BeautifulSoup(page.text, 'html.parser')
     container = soup.find_all("ul", class_="o-chart-results-list-row")
     for item in container:
+        list = [ ]
         rank = item.find("span", class_="a-font-primary-bold-l").text
         song = item.find("h3", class_="c-title").text
         artist = item.find("span", class_="u-max-width-230@tablet-only").text
-        list.append(rank)
-        list.append(song)
-        list.append(artist)
-        print(list)
+        list.append(rank.strip())
+        list.append(song.strip())
+        list.append(artist.strip())
+        #this is to add the current year to entries from this year
+        if '2' not in chart_url:
+            list.append("2022")
+        else:
+            list.append(chart_url[1:5])
+        big_list.append(list)
+    return big_list
 
-#def write_to_csv(url_list):
-    #chart_writer = csv.writer(myfile)
-    #chart_writer.writerow(["rank", "song", "artist"])
-    #for url in url_list:
-        #results = scrape_chart(url)
-        #chart_writer.writerow(results)
+def write_to_csv(url_list):
+    chart_writer = csv.writer(myfile)
+    chart_writer.writerow(["rank", "song", "artist", "year"])
+    for url in url_list:
+        results = scrape_chart(url)
+        chart_writer.writerow(results)
 
-#write_to_csv(url_list)
 scrape_chart(test_url)
+write_to_csv(test_url)
+write_to_csv(url_list)
 myfile.close()
